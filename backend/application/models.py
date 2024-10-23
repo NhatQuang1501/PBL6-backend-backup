6227b847f9381ffa3acaae95fdcb21bd88c4e4b3
NhatQuang1501 <nhatquang1512003@gmail.com>
2024-10-24 02:06:17 +0700

    Revert "test git repo"
    done testing
    This reverts commit 50ca077157117cefdd5f39986e6cae78134f1ef7.

--- backend/application/models.py
+++ backend/application/models.py
@@ -43,8 +43,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # def __str__(self):
-    #     return f"{self.post_id} - {self.title}"
+    def __str__(self):
+        return f"{self.post_id} - {self.title}"
 
 
 # class Negotiation(models.Model):
